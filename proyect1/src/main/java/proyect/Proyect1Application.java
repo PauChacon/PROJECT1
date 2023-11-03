c33ed5628e1754303546c6d06a98df0c0d07ff1c
FGM75 <fgonzalezm75@gmail.com>
2023-11-03 16:05:00 +0100

    codigo api

---
+++ proyect1/src/main/java/proyect/Proyect1Application.java
@@ -0,0 +1,13 @@
+package proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyect1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyect1Application.class, args);
+	}
+
+}
